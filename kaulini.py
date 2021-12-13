f6f4e0ed7bbb003e6e1bfecc6f7a3bd6f23319c8
aleksandrs12 <90208363+aleksandrs12@users.noreply.github.com>
2021-12-13 14:27:40 +0200

    Add files via upload

---
+++ kaulini.py
@@ -0,0 +1,8 @@
+import math
+
+n = int(input('cik kaulinu? '))
+
+print('uzmestie skaili ir visi para vai nepara skaitli: ' + str(math.pow(0.5 , n)))
+
+print('visi skaili ir vienadi: ' + str(math.pow(0.17 , n-1)))
+
